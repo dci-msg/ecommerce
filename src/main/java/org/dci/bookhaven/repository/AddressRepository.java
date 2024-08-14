420400858f7e58a1d9e12669df72b6ef3d9c2530
Gizem <zuehre.turhan@dci-student.org>
2024-08-14 09:15:01 +0200

    saved

--- src/main/java/org/dci/bookhaven/repository/AddressRepository.java
+++ src/main/java/org/dci/bookhaven/repository/AddressRepository.java
@@ -2,7 +2,6 @@ package org.dci.bookhaven.repository;
 
 
 
-import org.dci.bookhaven.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
