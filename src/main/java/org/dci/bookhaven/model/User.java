b29f290142ea4c167ff2a8e650eb91c52bcccff5
dci-student <Terissa>
2024-08-28 08:29:19 +0200

    changes made in User Class

--- src/main/java/org/dci/bookhaven/model/User.java
+++ src/main/java/org/dci/bookhaven/model/User.java
@@ -2,23 +2,29 @@ package org.dci.bookhaven.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(unique = true)
     private String email;
+
     @NotEmpty
     private String password;
-
-    @Column(name = "is_active")
     private boolean isActive;
 
     @CreationTimestamp
@@ -29,79 +35,5 @@ public class User {
     @JoinColumn(name = "userTypeId", referencedColumnName = "userTypeId")
     private UserType userType;
 
-    //constructors
-    public User() {
-    }
 
-    public User(Long id, String email, String password, boolean isActive, LocalDateTime registrationDate, UserType userType) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.isActive = isActive;
-        this.registrationDate = registrationDate;
-        this.userType = userType;
-    }
-
-    //getters setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long userId) {
-        this.id = userId;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setActive(boolean active) {
-        isActive = active;
-    }
-
-    public LocalDateTime getRegistrationDate() {
-        return registrationDate;
-    }
-
-    public void setRegistrationDate(LocalDateTime registrationDate) {
-        this.registrationDate = registrationDate;
-    }
-
-    public UserType getUserType() {
-        return userType;
-    }
-
-    public void setUserType(UserType userType) {
-        this.userType = userType;
-    }
-
-    //toString
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "userId=" + id +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", isActive=" + isActive +
-                ", registrationDate=" + registrationDate +
-                ", userType=" + userType +
-                '}';
-    }
 }
